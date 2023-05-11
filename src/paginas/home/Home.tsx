c4dd00334cf949e98cb1a58ebe0ce092beda059c
chris-kauffmann <chris_kauffmann@hotmail.com>
2023-05-11 15:19:19 -0300

    iniciando o front

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+/*function para criar o componente inicial home */
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo,
+        vitae iaculis nisl.Em pé sem cair, deitado sem dormir, sentado sem
+        cochilar e fazendo pose.Nullam volutpat risus nec leo commodo, ut
+        interdum diam laoreet. Sed non consequat odio.Mé faiz elementum
+        girarzis, nisi eros vermeio. In elementis mé pra quem é amistosis quis
+        leo.Per aumento de cachacis, eu reclamis.Quem num gosta di mé, boa
+        gentis num é.Delegadis gente finis, bibendum egestas augue arcu ut est.
+        Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
+      </p>
+    </>
+  );
+}
+
+export default Home;
